15be3eef9d5e5c80a6d210ad84958e492364d2a7
Jon Champagne <jayseejc3191@gmail.com>
2015-04-22 16:07:41 -0400

    Added skeletons of event classes

--- brewchat/src/main/java/com/example/brewchat/ui/MainActivity.java
+++ brewchat/src/main/java/com/example/brewchat/ui/MainActivity.java
@@ -1,9 +1,11 @@
-package com.example.brewchat;
+package com.example.brewchat.ui;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.example.brewchat.R;
 
 
 public class MainActivity extends ActionBarActivity {
