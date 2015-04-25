a9ddad961db568ff3e58934833267f9b8379c64a
Ryanm14 <ryanm1114@gmail.com>
2015-04-24 21:45:56 -0400

    Add Authentication

---
+++ brewchat/src/main/java/com/example/brewchat/fragments/AuthenticationModelFragment.java
@@ -0,0 +1,88 @@
+package com.example.brewchat.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+
+import com.example.brewchat.activities.LoginActivity;
+import com.quickblox.auth.QBAuth;
+import com.quickblox.auth.model.QBSession;
+import com.quickblox.chat.QBChatService;
+import com.quickblox.core.QBEntityCallbackImpl;
+import com.quickblox.core.QBSettings;
+import com.quickblox.core.exception.QBResponseException;
+import com.quickblox.users.QBUsers;
+import com.quickblox.users.model.QBUser;
+
+import java.util.List;
+
+/**
+ * Created by Ryanm14 on 4/24/2015.
+ */
+public class AuthenticationModelFragment extends Fragment{
+
+    private static final String TAG = "AuthModelFragment";
+    private static final String APP_ID = "22306";
+    private static final String AUTH_KEY = "S2pgg2kV3cfLUAm";
+    private static final String AUTH_SECRET = "am-HzubL-aMmekY";
+    QBChatService chatService;
+
+   public AuthenticationModelFragment(){
+    }
+
+    public static AuthenticationModelFragment newInstance(Bundle savedInstanceState) {
+        return new AuthenticationModelFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+        QBSettings.getInstance().fastConfigInit(APP_ID, AUTH_KEY, AUTH_SECRET);
+        Log.d(TAG, "Initializing");
+        if (!QBChatService.isInitialized()) {
+            QBChatService.init(getActivity());
+            Log.d(TAG, "isInitialized");
+        }
+        chatService = QBChatService.getInstance();
+    }
+
+    public void login(String username, String password){
+        Log.d(TAG,"Creating User");
+        final QBUser user = new QBUser(username, password);
+        try {
+            QBUsers.signIn(user);
+        } catch (QBResponseException e) {
+            e.printStackTrace();
+        }
+        Log.d(TAG,"User Created");
+        QBAuth.createSession(user, new QBEntityCallbackImpl<QBSession>() {
+            @Override
+            public void onSuccess(QBSession session, Bundle params) {
+                Log.d(TAG,"Creating Session");
+                user.setId(session.getUserId());
+
+                chatService.login(user, new QBEntityCallbackImpl() {
+                    @Override
+                    public void onSuccess() {
+                        Log.d(TAG, "User Authenticated");
+                        LoginActivity login = (LoginActivity) getActivity();
+                        login.goToChatFragment();
+                    }
+
+                    @Override
+                    public void onError(List errors) {
+                        Log.e(TAG, "Error in Authentication");
+                        //Add toast to Login Activity
+                    }
+                });
+            }
+
+            @Override
+            public void onError(List<String> errors) {
+                Log.e(TAG, "Check credentials");
+            }
+        });
+
+    }
+}
