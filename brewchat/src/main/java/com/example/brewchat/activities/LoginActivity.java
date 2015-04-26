e0150a7c13c87c46af6c4da4c9be1409944140d7
Ryanm14 <ryanm1114@gmail.com>
2015-04-25 20:05:04 -0400

    Fix lint error

--- brewchat/src/main/java/com/example/brewchat/activities/LoginActivity.java
+++ brewchat/src/main/java/com/example/brewchat/activities/LoginActivity.java
@@ -2,25 +2,27 @@ package com.example.brewchat.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.brewchat.Application;
 import com.example.brewchat.R;
-import com.example.brewchat.UserLoggedEvent;
+import com.example.brewchat.events.UserLoggedEvent;
+import com.example.brewchat.fragments.LoginFragment;
+import com.example.brewchat.fragments.RegisterDialogFragment;
+import com.example.brewchat.interfaces.LoginListener;
+import com.example.brewchat.interfaces.RegisterDialogListener;
 
-import butterknife.ButterKnife;
-import butterknife.InjectView;
 import de.greenrobot.event.EventBus;
 
 
 public class LoginActivity extends AppCompatActivity implements RegisterDialogListener,
         LoginListener{
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -56,12 +58,12 @@ public class LoginActivity extends AppCompatActivity implements RegisterDialogLi
     }
 
     //Simply for navigation purposes during Dev
-    public void buttonAuth(View view) {
-        ((Application)getApplication()).getChatService().login(username.getText().toString(), password.getText().toString());
+    public void buttonAuth(String username, String password) {
+        ((Application)getApplication()).getChatService().login(username, password);
     }
 
     @SuppressWarnings("unused")
-    public void goToChatFragment(UserLoggedEvent event) {
+    public void onEvent(UserLoggedEvent event) {
         Intent intent = new Intent(this, ChatManagerActivity.class);
         startActivity(intent);
     }
@@ -69,10 +71,10 @@ public class LoginActivity extends AppCompatActivity implements RegisterDialogLi
     public void error(){
         Toast.makeText(LoginActivity.this, "Error in logging in", Toast.LENGTH_LONG).show();
     }
-    
+
     @Override
     public void onCreateAccount(String username, String password) {
-        ((Application) getApplication()).getChatService().register(username, password);
+        ((Application)getApplication()).getChatService().register(username, password);
     }
 
     public void showRegisterDialog(View view) {
