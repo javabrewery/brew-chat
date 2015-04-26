e50874cef7ddfe90633cf35457d244e5d501ee81
Josh <jephillips@cohesionforce.com>
2015-04-26 15:02:48 -0500

    Moved Eventbus reg/unreg to Start/Stop

--- brewchat/src/main/java/com/example/brewchat/activities/LoginActivity.java
+++ brewchat/src/main/java/com/example/brewchat/activities/LoginActivity.java
@@ -38,14 +38,14 @@ public class LoginActivity extends AppCompatActivity implements RegisterDialogLi
     }
 
     @Override
-    public void onResume() {
-        super.onResume();
+    protected void onStart() {
+        super.onStart();
         EventBus.getDefault().register(this);
     }
 
     @Override
-    public void onPause() {
-        super.onPause();
+    public void onStop() {
+        super.onStop();
         EventBus.getDefault().unregister(this);
     }
 
