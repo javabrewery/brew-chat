b2585f9cae85ebc83c94ed6ccb09e03efc2dbf87
Jon Champagne <jayseejc3191@gmail.com>
2015-04-29 21:14:46 -0400

    Created contacts view

--- brewchat/src/main/java/com/example/brewchat/activities/ContactsActivity.java
+++ brewchat/src/main/java/com/example/brewchat/activities/ContactsActivity.java
@@ -3,6 +3,8 @@ package com.example.brewchat.activities;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.example.brewchat.Application;
 import com.example.brewchat.R;
@@ -52,6 +54,28 @@ public class ContactsActivity extends AppCompatActivity {
     protected void onStop() {
         super.onStop();
         EventBus.getDefault().unregister(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_contacts, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     public void onEvent(UsersLoadedEvent event) {
