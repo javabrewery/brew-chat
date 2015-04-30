8243a9a82d2c72682f806cfecd3ed951ebd0341c
Jon Champagne <jayseejc3191@gmail.com>
2015-04-29 21:22:31 -0400

    Created default user icon

--- brewchat/src/main/java/com/example/brewchat/adapters/ContactsRecyclerAdapter.java
+++ brewchat/src/main/java/com/example/brewchat/adapters/ContactsRecyclerAdapter.java
@@ -44,7 +44,7 @@ public class ContactsRecyclerAdapter extends RecyclerView.Adapter<ContactsRecycl
         else name = entry.getLogin();
         holder.contactName.setText(name);
 
-        // TODO look into contact images. Maybe via gravitar? https://en.gravatar.com/site/implement
+        // TODO look into contact images. Maybe via gravitar? https://en.gravatar.com/site/implement/
         holder.contactImage.setImageResource(R.drawable.ic_account_box_grey_800_48dp);
     }
 
