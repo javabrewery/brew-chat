e4908cc5db5ac5185af3e1467cd4027d6eba5964
arreche <arreche@gmail.com>
2015-05-25 01:29:30 +0100

    Setup spinner to select a chat service

--- brewchat/src/main/java/com/example/brewchat/fragments/LoginFragment.java
+++ brewchat/src/main/java/com/example/brewchat/fragments/LoginFragment.java
@@ -4,20 +4,29 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.ToggleButton;
 
 import com.example.brewchat.Application;
 import com.example.brewchat.R;
 import com.example.brewchat.chatservices.FakeChatService;
+import com.example.brewchat.chatservices.IRCChatService;
 import com.example.brewchat.chatservices.QBChatService;
 import com.example.brewchat.interfaces.LoginListener;
+
+import java.util.Arrays;
+import java.util.List;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -31,6 +40,8 @@ import butterknife.OnClick;
 
 public class LoginFragment extends Fragment {
 
+    @InjectView(R.id.fake_toggleButton)
+    Spinner chatServiceSpinner;
     @InjectView(R.id.login_button)
     Button loginButton;
     @InjectView(R.id.login_username_edittext)
@@ -71,9 +82,39 @@ public class LoginFragment extends Fragment {
         View view = inflater.inflate(R.layout.login_fragment, container, false);
         ButterKnife.inject(this, view);
 
+        ((AppCompatActivity)getActivity()).getSupportActionBar().hide();
+
         // for testing, set default values to save typing
         usernameEditText.setText("user1");
         passwordEditText.setText("12345678");
+
+        String[] chatServices = {"QuickBlox", "IRC", "Fake"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
+                                                                android.R.layout.simple_list_item_1,
+                                                                chatServices);
+
+        chatServiceSpinner.setAdapter(adapter);
+        chatServiceSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                if (i == 0) {
+                    Application.chatService = new QBChatService(getActivity().getApplicationContext(),
+                            getString(R.string.app_id),
+                            getString(R.string.auth_key),
+                            getString(R.string.auth_secret));
+                } else if (i == 1) {
+                    Application.chatService = new IRCChatService();
+                } else {
+                    Application.chatService = new FakeChatService();
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
 
         return view;
     }
@@ -94,18 +135,4 @@ public class LoginFragment extends Fragment {
         usernameEditText.setText(username);
     }
 
-    @OnClick(R.id.fake_toggleButton)
-    @SuppressWarnings("unused")
-    public void onFakeToggleClicked(View view) {
-        boolean on = ((ToggleButton) view).isChecked();
-
-        if (on) {
-            Application.chatService = new FakeChatService();
-        } else {
-            Application.chatService = new QBChatService(getActivity().getApplicationContext(),
-                    getString(R.string.app_id),
-                    getString(R.string.auth_key),
-                    getString(R.string.auth_secret));
-        }
-    }
 }
